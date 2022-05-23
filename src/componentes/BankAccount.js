810d529bea3af2f212eb9ddc46a594031dc369ae
jorge machado <jrms_jorgesilva@hotmail.com>
2022-05-23 17:12:46 -0300

    integração redux para componente da BankAccount

--- src/componentes/BankAccount.js
+++ src/componentes/BankAccount.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { TextField, Button, Stack } from '@mui/material'
-import { FaCreditCard, FaMoneyCheck } from 'react-icons/fa'
+import { FaCreditCard, FaMoneyCheck, FaTrophy, FaUser, FaPass } from 'react-icons/fa'
 import { depositAction, withdrawAction } from './Redux/Actions/BankAccountActions';
 
 const BankAccount = () => {
 
-    const account = useSelector(state => state.BankAccountReducer.account);
+    // seta account com dados do reducers conta bancaria
+    const account = useSelector(state => state.BankAccountReducer);
 
-    console.log(account, account.saldo);
-
+    // seta dispatch
     const dispatch = useDispatch();
 
+    // seta value para manter o valor da conta pelo campo input
     const [value, setValue] = useState(() => 0);
 
     return(
@@ -20,13 +21,24 @@ const BankAccount = () => {
             <p>
                 <small className='m-3'>O banco digital que veio para mudar tudo</small>
             </p>
-            <Stack direction='row' width='100%' spacing={2} margin={4}>
+            <Stack direction='column' spacing={1} margin={2}>
+                <h3>Bem vindo, <span className='app__highlight'>{account.name} {account.lastName}</span></h3>
+                <p>
+                    <small>Cliente {account.typeAccount}</small>
+                    {
+                        account.typeAccount === 'premium' 
+                        ? (<FaTrophy style={{ color: 'lime', marginTop: '5px' }} />) 
+                        : (<FaUser style={{ color: 'lime', marginTop: '15px' }} />)
+                    }
+                </p>
+            </Stack>
+            <Stack direction='row' width='100%' spacing={2} margin={2}>
                 <TextField 
                     type='number'
-                    id='outlined-basic'
+                    variant='filled'
                     placeholder='Valor'
                     className='app__input'
-                    onChange={(e) => setValue(e.target.value)}
+                    onChange={(e) => setValue(parseInt(e.target.value))}
                 />
                 <Button 
                     variant='contained' 
--- src/componentes/Redux/Reducers/BankAccountReducer.js
+++ src/componentes/Redux/Reducers/BankAccountReducer.js
@@ -1,28 +1,23 @@
 import { DEPOSIT, WITHDRAW } from '../Actions/BankAccountActions'
 
 const INITIAL_SALDO = {
-    account: {
-        id: 1,
-        name: 'Matteus',
-        lastName: 'Santhiago Albuquerque',
-        saldo: 324.34,
-        typeAccount: 'premium'
-    }
+    saldo: 324.67,
+    id: 1,
+    name: 'Matteus',
+    lastName: 'Santhiago Albuquerque',
+    typeAccount: 'premium',
+    credito: false,
+    débito: true,
+    serasa: false
 }
 
 
 const BankAccountReducer = (state = INITIAL_SALDO, action) => {
     switch(action.type) {
         case DEPOSIT:
-            // return {
-            //     state.account: state + action.payload
-            // }
-            console.log("action.payload", action.payload);
+            return { ...state, saldo: state.saldo + action.payload }
         case WITHDRAW:
-            return {
-                ...state,
-                saldo: state.saldo - action.payload
-            };
+            return { ...state, saldo: state.saldo - action.payload }
         default:
             return state;
     }
