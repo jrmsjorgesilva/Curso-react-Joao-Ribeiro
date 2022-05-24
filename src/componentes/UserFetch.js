ae11d32dca47a44c725ae9be36fb4ec3bb440525
jorge machado <jrms_jorgesilva@hotmail.com>
2022-05-24 11:09:17 -0300

    finalizado componente de buscas a API dummyjson com usuarios e frases. Correção de bugs relacionados a consulta na API

--- src/componentes/UserFetch.js
+++ src/componentes/UserFetch.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { List, ListItem, ListItemButton, ListItemText } from '@mui/material'
+import { FaTrash, FaSearch, FaStar, FaUser } from 'react-icons/fa'
 
 const UserFetch = () => {
 
     const [fetchData, setFetchData] = useState(() => []);
 
-    const [select, setSelect] = useState(() => 'quotes');
+    const [select, setSelect] = useState(() => 'users');
 
     console.log(select)
 
@@ -14,6 +15,7 @@ const UserFetch = () => {
     // const subject = ['quotes', 'users']
 
     useEffect(() => {
+
         if (select === 'quotes') {
             fetch(`${API_URL}quotes`)
             .then(response => response.json())
@@ -28,7 +30,7 @@ const UserFetch = () => {
             .then(response => response.json())
             .then(json => {
                 console.log(json.users);
-                setFetchData(json.users);
+                setFetchData(json.users.map(elem => elem.firstName));
             })
         }
 
@@ -43,19 +45,42 @@ const UserFetch = () => {
             <h1 className='app__title center'>Frases Ins Piradoras</h1>
             <select onChange={(e) => handleChange(e)}>
                 <option value='quotes'>Quotes</option>
-                <option value='users'>users</option>
+                <option value='users'>Users</option>
             </select>
             {fetchData.map(data => 
-                    <>
-                        <List key={data.id}>
-                            <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemText primary={Object.entries(data)} />
-                            </ListItemButton>
-                            </ListItem>
-                        </List>
-                    </>
-                )}
+                <>
+                    <List key={data.id}>
+                        <ListItem disablePadding>
+                        <ListItemButton>
+                            {select === 'users' &&
+                                <FaUser 
+                                    role='button' 
+                                    style={{ 
+                                        color: 'gray', 
+                                        margin: '10px' 
+                                    }} 
+                                />
+                            }
+                            <ListItemText primary={Object.values(data)} />
+                            <FaTrash 
+                                role='button' 
+                                style={{ 
+                                    color: 'firebrick', 
+                                    margin: '10px' 
+                                }} 
+                            />
+                            <FaStar 
+                                role='button' 
+                                style={{ 
+                                    color: '#dcdf00', 
+                                    margin: '10px' 
+                                }} 
+                            />
+                        </ListItemButton>
+                        </ListItem>
+                    </List>
+                </>
+            )}
         </div>
     )
 }
